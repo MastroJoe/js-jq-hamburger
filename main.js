538e479a6bf1faecfeb666c2ae970f7ecf851c4b
Giorgio <giorgio.pignattari@gmail.it>
2021-03-10 16:28:22 +0100

    finito js con hide/show

---
+++ main.js
@@ -0,0 +1,18 @@
+// Consegna
+// mostrare / nascondere il menu principale (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
+// creo 2 variabili, una per aprire e una per chiudere il menu
+var open = $(".header-right a");
+var close = $(".hamburger-menu a");
+
+// richiamo il menu con una variabile
+var hamburgerMenu = $(".hamburger-menu");
+
+// funzione show per mostrare il menu al click
+open.click(function() {
+  hamburgerMenu.show();
+});
+
+// funzione hide per nascondere il menu al click
+close.click(function() {
+  hamburgerMenu.hide();
+});
