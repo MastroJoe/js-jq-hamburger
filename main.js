41d9e2ee382774d34edb30437e31c8d6bd1209d6
Giorgio <giorgio.pignattari@gmail.it>
2021-03-11 15:30:53 +0100

    finito js con add/removeClass

--- main.js
+++ main.js
@@ -7,12 +7,22 @@ var close = $(".hamburger-menu a");
 // richiamo il menu con una variabile
 var hamburgerMenu = $(".hamburger-menu");
 
-// funzione show per mostrare il menu al click
-open.click(function() {
-  hamburgerMenu.show();
+// // funzione show per mostrare il menu al click
+// open.click(function() {
+//   hamburgerMenu.show();
+// });
+//
+// // funzione hide per nascondere il menu al click
+// close.click(function() {
+//   hamburgerMenu.hide();
+// });
+
+// OPPURE
+// funzione addClass e removeClass che reagiscono al click per aggiungere e togliere una
+$(".header-right > a > i").click(function() {
+  hamburgerMenu.addClass("active");
 });
 
-// funzione hide per nascondere il menu al click
-close.click(function() {
-  hamburgerMenu.hide();
+$(".hamburger-menu > a").click(function() {
+  hamburgerMenu.removeClass("active");
 });
